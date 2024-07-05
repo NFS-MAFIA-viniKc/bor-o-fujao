ef3802adaf4c0d69676cbbf42cba21a09afa2d5c
viniMporto <162714936+NFS-MAFIA-viniKc@users.noreply.github.com>
2024-07-05 10:34:28 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,16 @@
+function sim() {
+    alert(" esta comprovado, Augusto é gay 🤢👨🏿‍🤝‍👨🏿👨🏾‍🤝‍👨🏻(¬‿¬) ");
+    // redireciona para um URL após clicar no SIM
+}
+
+function desvia(btn) {
+    // btn declarado na função
+    btn.style.position = 'absolute';
+    btn.style.bottom = geraPosicao(10, 90);
+    btn.style.left = geraPosicao(10, 90);
+    console.log('opa, desviei...');
+}
+
+function geraPosicao(min, max) {
+    return (Math.random() * (max - min) + min) + "%";
+}
